ab22a96b751d0686ddc8806f6236d798d978ce82
Yuri <yuri.passamonti@blastingnews.com>
2020-01-25 17:36:16 +0100

    removed devtool

--- src/store/index.js
+++ src/store/index.js
@@ -3,4 +3,4 @@ import thunk from 'redux-thunk'
 import rootReducer from "./reducer"
 
 
-export default  createStore(rootReducer, compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+export default  createStore(rootReducer, compose(applyMiddleware(thunk)))
